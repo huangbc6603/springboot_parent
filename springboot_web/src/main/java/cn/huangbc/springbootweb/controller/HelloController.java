5573b7804733500a6d6b64f6fa12cb70c9d6ea9d
huangbc <huangbc_6603@163.com>
2021-12-19 18:22:44 +0800

    Initial commit

---
+++ springboot_web/src/main/java/cn/huangbc/springbootweb/controller/HelloController.java
@@ -0,0 +1,20 @@
+package cn.huangbc.springbootweb.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author huangbc
+ * @version 1.0
+ * @date 2021/12/19 17:53
+ */
+@RestController
+@RequestMapping("Test")
+public class HelloController {
+
+    @RequestMapping("sayHello")
+    public String sayHello(){
+        return "hello Word...............hahaha";
+    }
+
+}
