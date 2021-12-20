38d299c406645e77fca11c5b0128dc1dce37ce0d
huangbc <huangbc_6603@163.com>
2021-12-20 21:18:21 +0800

    init druid http://localhost:8888/druid/sql.html

--- springboot_web/src/main/java/cn/huangbc/springbootweb/SpringbootWebApplication.java
+++ springboot_web/src/main/java/cn/huangbc/springbootweb/SpringbootWebApplication.java
@@ -1,9 +1,15 @@
 package cn.huangbc.springbootweb;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.stereotype.Component;
 
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
+@ServletComponentScan
 public class SpringbootWebApplication {
 
     public static void main(String[] args) {
